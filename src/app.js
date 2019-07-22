6f7a2b576236d9a9ecc4461e5e59dcf8917adec5
wades1248 <wades1248@gmail.com>
2019-07-22 15:41:11 -0500

    fix, fix, fix

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const {NODE_ENV} = require('./config')
+const {CLIENT_ORIGIN} = require('./config');
 
 const app = express()
 
@@ -13,9 +14,7 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(
-    cors({
-        origin: ClIENT_ORIGIN
-    }
+    cors(
 ))
 app.use(helmet())
 
